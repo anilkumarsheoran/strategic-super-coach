f682b75d0453b0e4581ddd8d44d6a71b8a647d13
anil sheoran <anil.sheoran@akqa.com>
2023-05-26 15:16:56 +1000

    add skill page

---
+++ src/pages/fitness.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Fitness() {
+  
+
+  return (   
+    <div>
+    <Link href="/chat">
+      <img className='mx-auto' src="/AIcoach.png" />
+    </Link>
+   </div>
+  )
+}
